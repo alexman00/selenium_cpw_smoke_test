c2471b596e8aaf450f6f70d382f09a7b69cd03dd
Alexman <alexandru.mandache.work@gmail.com>
2020-02-05 14:39:28 +0200

    Initial commit

---
+++ SampleProject/GoogleSearchTest.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import unittest
+import HtmlTestRunner
+
+
+class GoogleSearch(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(
+            executable_path="/Users/alexandru.mandache/PycharmProjects/cpw_smoke_test/Drivers/chromedriver")
+
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+
+
+    def test_search_automation(self):
+        self.driver.get("https://www.google.com")
+
+        self.driver.find_element_by_name("q").send_keys("Automation")
+        self.driver.find_element_by_class_name("hsuHs").click()
+        print(self.driver.title)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test Completed")
+
+
+
+
+if __name__ =='__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/Users/alexandru.mandache/PycharmProjects/cpw_smoke_test/Reports'))
---
+++ SampleProject/POMDemo/Tests/LoginTest.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+import time
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from SampleProject.POMDemo.Pages.LoginPage import LoginPage
+from SampleProject.POMDemo.Pages.HomePage import HomePage
+import HtmlTestRunner
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.driver = webdriver.Chrome(
+            executable_path="/Users/alexandru.mandache/PycharmProjects/cpw_smoke_test/Drivers/chromedriver")
+
+        cls.driver.implicitly_wait(10)
+
+    def test_login(self):
+        driver = self.driver
+
+        driver.get("https://opensource-demo.orangehrmlive.com")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+        #
+        #
+        # self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        #
+        # self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        #
+        # self.driver.find_element_by_id("btnLogin").click()
+        #
+        # self.driver.find_element_by_class_name("panelTrigger").click()
+        # print("yup")
+        #
+        # self.driver.find_element_by_link_text("Logout").click()
+
+        time.sleep(3)
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/Users/alexandru.mandache/PycharmProjects/cpw_smoke_test/Reports'))
